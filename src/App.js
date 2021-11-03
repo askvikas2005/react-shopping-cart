e835f2a58db15bf55471b968d61ea2d7acef1c4a
Vikas <askvikas2005@gmail.com>
2021-11-03 17:00:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <div className='grid-container'>
+      <header>
+       <a href="/">header</a>
+      </header>
+      <main>
+        main
+      </main>
+      <footer>
+        footer
+      </footer>
+    </div>
+  );
+}
+
+export default App;
